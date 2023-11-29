2eac142ee3f224bd76e1cda9667030b7c69283da
Danil-Sharipov <121438053+Danil-Sharipov@users.noreply.github.com>
2023-11-29 15:40:31 +0300

    Second update

--- test/test.py
+++ test/test.py
@@ -6,4 +6,8 @@ for i in containers:
 		exit_code,output = i.exec_run("/opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --zookeeper localhost:2181 --list",user='0')
 		if exit_code != 0:
 			print(output)
-			raise Explorer
+			raise Exception("kafka-topics not work")
+		exit_code,output = i.exec_run("/opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --zookeeper localhost:2181 --describe --topic MyTopic",user='0')
+		if exit_code != 0:
+			print(output)
+			raise Exception("kafka-topics not work")
